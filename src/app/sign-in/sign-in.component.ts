717528f8e3e18864a7ebb171b3dabc6ad7c5024e
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-05-03 17:36:58 +0300

    added sign in component with validation

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  signInForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+
+    this.signInForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    })
+  }
+
+  onSubmit(): void {
+    if (this.signInForm.valid) {
+      this.router.navigate(['/home']);
+    } else {
+      this.signInForm.markAllAsTouched();
+    }
+  }
 }
