444b5c4a69582ac38197a36fc3d8bc916de68f17
Mohamad Youness <pixlerfrost@gmail.com>
2024-05-05 14:20:18 +0300

    Added validation to sign-up form + design changes

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  signUpForm : FormGroup;
 
+  constructor(private router : Router)
+  {
+    this.signUpForm = new FormGroup({
+      email : new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required]),
+      firstname: new FormControl("", [Validators.required]),
+      lastname: new FormControl("", [Validators.required]),
+      termsAndConditions: new FormControl(false, [Validators.requiredTrue])
+    })
+  }
+  resetForm() : void 
+  {   
+    this.signUpForm.reset();
+  }
+
+  onSubmit(): void
+  {
+    if (this.signUpForm.valid)
+    {
+      this.router.navigate(["/"]);
+      this.resetForm();
+    }
+    else
+      this.signUpForm.markAllAsTouched();
+  }
 }
